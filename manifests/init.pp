7bbbb2e14c7fccf3bc7d05b3c85140423cdf8b77
Eric Putnam <putnam.eric@gmail.com>
2017-10-23 14:13:31 -0700

    (FM-6530) created init and params pattern with warning and fail messages

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,9 +2,23 @@
 #
 # A description of what this class does
 #
-# @summary A short summary of the purpose of this class
+# @summary Sample manifest for demoing i18n functionality in Puppet
 #
 # @example
 #   include i18ndemo
-class i18ndemo {
+class i18ndemo (
+  $filename = 'i18ndemofile.txt',
+  $param1   = true,
+  $param2   = 'param2',
+  $param3   = $i18ndemo::params::param3
+) inherits i18ndemo::params {
+  if $param1 == true {
+    warning("Creating ${filename} file")
+    file { $filename:
+      ensure  => present,
+      content => 'some i18n demo content for no reason',
+    }
+  } else {
+    fail("Failed to create ${filename} file")
+  }
 }
---
+++ manifests/params.pp
@@ -0,0 +1,12 @@
+# i18ndemo::params
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include i18ndemo::params
+class i18ndemo::params (
+  $param3 = 'param3'
+){
+}
