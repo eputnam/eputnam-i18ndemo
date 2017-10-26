54a21a56d685430c846198b4b5bafe4230f83670
Eric Putnam <putnam.eric@gmail.com>
2017-10-26 13:57:28 -0700

    (FM-6527) add license file and update fact for rubocop

--- lib/facter/i18ndemo_fact.rb
+++ lib/facter/i18ndemo_fact.rb
@@ -1,6 +1,7 @@
 Facter.add('i18ndemo_fact') do
   setcode do
     fact_var = 'fact'
+    # rubocop:disable Style/StringLiterals
     raise _("i18ndemo_%{fact} is a raise from a custom %{fact} from eputnam-i18ndemo") % { fact: fact_var }
   end
 end
