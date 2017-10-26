810eb02a3ba132ef131e346671128c86109e5223
Eric Putnam <putnam.eric@gmail.com>
2017-10-26 14:19:23 -0700

    (FM-6531) add function and test for function

---
+++ lib/puppet/functions/happyfuntime.rb
@@ -0,0 +1,12 @@
+Puppet::Functions.create_function(:happyfuntime) do
+  dispatch :happyfuntime do
+    param 'String', :value
+  end
+
+  def happyfuntime(value)
+    s = ""
+    s << "--*IT'S HAPPY FUN TIME*--"
+    s << value.to_s.reverse
+    s << "--*********************--"
+  end
+end
---
+++ spec/functions/happyfuntime_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe 'happyfuntime' do
+
+  it { is_expected.to run.with_params("hello") }
+
+end
