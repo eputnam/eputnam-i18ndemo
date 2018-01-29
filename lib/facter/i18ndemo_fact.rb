4aada693e27c34e55c458d1e57f04e3face255e2
Eric Putnam <putnam.eric@gmail.com>
2018-01-29 10:23:28 -0800

    removes openssl check from fact

--- lib/facter/i18ndemo_fact.rb
+++ lib/facter/i18ndemo_fact.rb
@@ -1,6 +1,5 @@
 Facter.add('i18ndemo_fact') do
   setcode do
-    opensslv = Facter::Util::Resolution.exec('openssl version')
-    raise _('i18ndemo_fact: this is a raise from a custom fact from eputnam-i18ndemo') if opensslv
+    raise _('i18ndemo_fact: this is a raise from a custom fact from eputnam-i18ndemo')
   end
 end
